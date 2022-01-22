fa4cf5cddf5258fa7bcff467b3f4776161b2ced1
pnjoroge54 <33970212+pnjoroge54@users.noreply.github.com>
2022-01-22 14:42:30 +0300

    Update files

---
+++ config.py
@@ -0,0 +1,7 @@
+TWITTER_USERNAMES = [
+                    'traderstewie',
+                    'the_chart_life',
+                    'canuck2usa',
+                    'sunrisetrader',
+                    'tmltrader'
+                    ]
